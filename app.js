cbec43a1984f4f2b1bd92b688cdbbf231c9e92c0
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2022-06-02 19:35:35 +0300

    Setup passport

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const connectDb = require('./database');
-const express = require('express');
+const connectDb = require("./database");
+const express = require("express");
+const passport = require("passport");
+const urlRoutes = require("./api/urls/urls.routes");
+const userRoutes = require("./api/users/users.routes");
 const app = express();
-const urlRoutes = require('./api/urls/urls.routes');
-const userRoutes = require('./api/users/users.routes');
 
 connectDb();
 app.use(express.json());
+app.use(passport.initialize());
 
-app.use('/urls', urlRoutes);
+app.use("/urls", urlRoutes);
 app.use(userRoutes);
 
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -20,11 +22,11 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
     error: {
-      message: err.message || 'Internal Server Error',
+      message: err.message || "Internal Server Error",
     },
   });
 });
 
 app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+  console.log("The application is running on localhost:8000");
 });
